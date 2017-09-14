c44a3b4dce6577dca954a3c801621d7f1b3eec33
shimengyv <2100782687@qq.com>
2017-09-14 15:50:19 +0800

    四步数组练习

---
+++ arr.js
@@ -0,0 +1,33 @@
+/**
+ * 2017-09-14   Shi Mengyv
+ */
+
+ var arr = ['a', 'b'];
+
+ arr.push('c', 1, 2);
+
+ console.log( arr );
+
+ console.log( `arr.pop() => [ ${arr.pop() } ]`);
+
+ console.log( arr );
+
+ /*********unshift shift**********/
+ var arr1 = [1, 2];
+
+ arr1.unshift(3, 4);
+
+ console.log( arr1 );
+
+ console.log( arr1.shift() );
+
+ console.log( arr1 );
+
+/*sort*/
+var arr2 = [1, 3, 5];
+
+console.log( arr2 );
+
+
+
+
