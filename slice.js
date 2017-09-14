e4af5060cc14eb90a1d25732eb3cb9c18d84cbc1
shimengyv <2100782687@qq.com>
2017-09-14 15:04:04 +0800

    截取数组的部分元素并返回一个新的数组

---
+++ slice.js
@@ -0,0 +1,19 @@
+/**
+ * 2017-09-14  Shi Mengyv
+ */
+
+var arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+
+console.log(`arr.slice(0) => [${arr.slice(0)}]` );
+
+console.log(`arr.slice(-1) => [ ${arr.slice(-1)} ]` );
+
+console.log(`arr.slice(-2, -1) => [ ${arr.slice(-2, -1)} ]` );
+
+console.log(`arr.slice(-2) => [ ${arr.slice(-2)} ]` );
+
+console.log(`arr.slice(0, -2) => [ ${arr.slice(0, -2)} ]` );
+
+console.log(`arr.slice(-4, 1) => [ ${arr.slice(-4, 1)} ]` );
+
+console.log(`arr.slice(8) = [ ${arr.slice(8)} ]` );
